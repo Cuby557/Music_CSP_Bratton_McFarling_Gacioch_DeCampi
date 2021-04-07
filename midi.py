5378357615af094c833cee6e0b0ca2805002fc09
jdecampi <jacobdecampi@gmail.com>
2021-04-07 17:32:24 -0500

    Create midi.py
    Python program that converts an array of integers to a midi file

---
+++ midi.py
@@ -0,0 +1,20 @@
+from midiutil import MIDIFile
+
+degrees  = [0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23, 24]  # melody.py output
+track    = 0
+channel  = 0
+time     = 0    # In beats
+duration = 1    # In beats
+tempo    = 60   # In BPM
+volume   = 100  # 0-127, as per the MIDI standard
+
+MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
+                      # automatically)
+MyMIDI.addTempo(track, time, tempo)
+
+for i, pitch in enumerate(degrees):
+    MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
+
+with open("melody.mid", "wb") as output_file:
+    MyMIDI.writeFile(output_file)
+
