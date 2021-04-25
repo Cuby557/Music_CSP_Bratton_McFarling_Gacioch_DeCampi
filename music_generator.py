a1fedeadcd28b1c58371544825304f8a69b57a58
Colby Bratton <70618665+Cuby557@users.noreply.github.com>
2021-04-24 21:22:29 -0500

    Update music_generator.py
    Added a second option for the mutation function, as well as I contained the MIDI file generation inside of a function so it is a bit less just floating in main. Lastly, you will most likely need to remove "Music_Generator_GA" in front of the melody in the imports section. I have to have this in my code due to my project file structure. Sorry for the inconvenience on that guys

--- music_generator.py
+++ music_generator.py
@@ -2,10 +2,10 @@
 @author: Colby Bratton, Jack McFarling, Jonathan Gacioch, Jacob DeCampi
 '''
 from pyeasyga import pyeasyga
-from melody import Melody
+from Music_Generator_GA.melody import Melody
 from midiutil import MIDIFile
-import numpy as np
 import random
+import numpy as np
 
 '''
 Creates initial individual for the GA to utilize.
@@ -13,6 +13,8 @@ Creates initial individual for the GA to utilize.
 Does so by taking a random melody, shuffling the
 melody, and using that as an individual in the GA
 '''
+
+
 def create_individual(data):
     individual = data[:]
     random.shuffle(individual)
@@ -70,22 +72,53 @@ def fitness (individual, data):
     return fitness
 
 '''
-Mutation Three: swapping two notes
-Mutation function that makes a simple mutation by selection a random
+Mutation Two and Three
+Mutation function Two makes a simple mutation by selecting a random
+note and incrementing/decrementing it according to the noteList. In
+effect, we are making the note higher or lower by one with each
+mutation.
+Mutation function Three makes a simple mutation by selecting a random
 note and swapping it with the note before it (or after it if it is the last
 note in the array)
 '''
 def mutate(individual):
+    ### choose random operation and random note to mutate
+    mutation_selection = random.randint(0, 1);
     mutate_index = random.randrange(len(individual) - 1)
     
-    if mutate_index == (len(individual) - 1):
-        ### swap last two notes
-        individual[mutate_index], individual[mutate_index - 1] = \
-        individual[mutate_index - 1], individual[mutate_index]
+    ### mutation two
+    if mutation_selection == 0:
+        ### get note's index in noteList
+        noteList = [0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23, 24]
+        noteList_index = noteList.index(individual[mutate_index])
+        
+        ### randomly choose to increment/decrement
+        mutation_selection = random.randint(0, 1);
+        
+        ### increment note according to noteList
+        if mutation_selection == 0:
+            if noteList_index + 1 >= len(noteList):
+                individual[mutate_index] = noteList[noteList_index - 1]
+            else:
+                individual[mutate_index] = noteList[noteList_index + 1]
+            
+        ### decrement note according to noteList
+        else:
+            if noteList_index - 1 > 0:
+                individual[mutate_index] = noteList[noteList_index - 1]
+            else:
+                individual[mutate_index] = noteList[noteList_index + 1]
+        
+    ### mutation three
     else:
-        ### swap at mutate_index with note in front of it
-        individual[mutate_index], individual[mutate_index + 1] = \
-        individual[mutate_index + 1], individual[mutate_index]
+        if mutate_index == (len(individual) - 1):
+            # ## swap first and last notes
+            individual[0], individual[mutate_index - 1] = \
+            individual[mutate_index - 1], individual[0]
+        else:
+            # ## swap at mutate_index with note in front of it
+            individual[mutate_index], individual[mutate_index + 1] = \
+            individual[mutate_index + 1], individual[mutate_index]
         
 '''
 Selection Function
@@ -95,7 +128,7 @@ may be passed back to the GA. If multiple Individuals
 have the same Fitness result, the first one is returned.
 '''
 def selection(population):
-    #print(population)
+    # print(population)
     best_individual = None
     best_fitness_result = 0
      
@@ -105,6 +138,31 @@ def selection(population):
             best_fitness_result = individual.fitness
               
     return best_individual
+
+
+def create_MIDI_file_for(name, melody):
+    degrees = melody.copy()  # melody.py output
+
+    for index in range(len(degrees)):
+        degrees[index] = int(degrees[index]) + 60
+
+    track = 0
+    channel = 0
+    time = 0  # In beats
+    duration = 1  # In beats
+    tempo = 100  # In BPM
+    volume = 100  # 0-127, as per the MIDI standard
+
+    # One track, defaults to format 1 (tempo track is created automatically)
+    MyMIDI = MIDIFile(1)
+    
+    MyMIDI.addTempo(track, time, tempo)
+
+    for i, pitch in enumerate(degrees):
+        MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
+
+    with open(name + ".mid", "wb") as output_file:
+        MyMIDI.writeFile(output_file)
          
 # Initial melody (nine notes) to utilize in GA
 beginning_melody = Melody(9)
@@ -115,7 +173,7 @@ data = beginning_melody.notes.copy()
 # Initialize the GA with the given parameters
 music_generator = pyeasyga.GeneticAlgorithm(data,
                                             population_size=200,
-                                            generations=100,
+                                            generations=200,
                                             crossover_probability=0,
                                             mutation_probability=0.1,
                                             elitism=True,
@@ -130,31 +188,15 @@ music_generator.selection_function = selection
 # Run that sucker!
 music_generator.run()
 
+# Retrieve optimal melody
+final_melody = music_generator.best_individual()
+
+# Create MIDI files for initial and final
+create_MIDI_file_for("initial", beginning_melody.notes)
+create_MIDI_file_for("final", final_melody[1])
+
 # Print beginning and ending melodies
 print("Initial Random Melody: ")
 print(beginning_melody.notes)
 print("Final Melody: ")
-print(music_generator.best_individual())
-
-best_temp = music_generator.best_individual()
-best_melody = best_temp[1]
-
-for index in range(len(best_melody)):
-    best_melody[index] = int(best_melody[index]) + 60
-
-track    = 0
-channel  = 0
-time     = 0    # In beats
-duration = 1    # In beats
-tempo    = 60   # In BPM
-volume   = 100  # 0-127, as per the MIDI standard
-
-MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
-                      # automatically)
-MyMIDI.addTempo(track, time, tempo)
-
-for i, pitch in enumerate(best_melody):
-    MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
-
-with open("melody.mid", "wb") as output_file:
-    MyMIDI.writeFile(output_file)
+print(final_melody)
